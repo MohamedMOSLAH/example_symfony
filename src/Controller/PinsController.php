e371bc15ad9567c05edf658748046b7bfa1da30c
MOHAMED <mohamedaminemoslah@gmail.com>
2020-12-28 00:03:59 +0100

    Show page refactoring and fix styling issue with text-break

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Form\PinType;
 use App\Repository\PinRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,14 +29,16 @@ class PinsController extends AbstractController
     /**
      * @Route("/pins/create", name="app_pins_create", methods="GET|POST")
      */
-    public function create(Request $request, EntityManagerInterface $em): Response
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepo): Response
     {   
         $pin = new Pin;
         $form = $this->createForm(PinType::class, $pin);
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid() ){          
+        if($form->isSubmitted() && $form->isValid() ){
+            $janeDoe = $userRepo->findOneBy(['email' => 'janedoe@example.com']);
+            $pin->setUser($janeDoe);          
             $em->persist($pin);
             $em->flush();
 
